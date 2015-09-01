2713b1dd08c29f74e833bf26881cad5bc360c59e
Spencer Kaiser <skaiser@me.com>
2015-08-31 21:26:33 -0500

    Creating objective-c application

---
+++ iOS/Sidekick/Sidekick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sidekick
+//
+//  Created by Spencer Kaiser on 8/31/15.
+//  Copyright (c) 2015 Sidekick Predictive Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
